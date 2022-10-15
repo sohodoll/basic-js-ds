6b777d17d45681dd0bd228fd42703f517d4bbd21
sohodoll <kovalevvtl@gmail.com>
2022-10-15 14:13:29 +0300

    feat: finish list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -14,24 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(list, value) {
-  let newList = [];
-  list.forEach(element => {
-    if (element != value) {
-      newList.push(element);
-    }
-  })
-  return newList;
+/**/
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
 
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+function removeKFromList(list, k) {
+  let array = [];
+  while(list) {
+    if (list.value != k && list.value != 'undefined') {
+      array.push(list.value);
+  }
+  list = list.next
+  }
+  return convertArrayToList(array);
+}
 module.exports = {
   removeKFromList
 };
